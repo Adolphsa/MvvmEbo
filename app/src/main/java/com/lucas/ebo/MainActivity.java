78a436de6b4bb993dc560f3c5d84127b92c50a15
lucas <chong.lu@enabot.com>
2020-04-01 16:20:05 +0800

    Initial commit

---
+++ app/src/main/java/com/lucas/ebo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lucas.ebo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Created by lucas
+ *
+ * Date: 2020/4/1 15:56
+ *
+ * Description: 
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
