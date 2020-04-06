d87c995b00c5bcf66737b11d50d79c21c36872be
lucas <chong.lu@enabot.com>
2020-04-06 15:15:42 +0800

    2020/04/06 1.log工具类添加

--- app/src/main/java/com/lucas/ebo/EboApplication.java
+++ app/src/main/java/com/lucas/ebo/EboApplication.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStore;
 import androidx.lifecycle.ViewModelStoreOwner;
 
+import com.lucas.architecture.utils.LogUtils;
 import com.lucas.architecture.utils.Utils;
 
 /**
@@ -31,7 +32,36 @@ public class EboApplication extends Application implements ViewModelStoreOwner {
         mAppViewModelStore = new ViewModelStore();
 
         Utils.init(this);
+        initLogUtils();
 
+    }
+
+    /**
+     * init LogUtils
+     */
+    private void initLogUtils()
+    {
+        LogUtils.Config config = LogUtils.init(this)
+                // 设置 log 总开关，包括输出到控制台和文件，默认开
+                .setLogSwitch(BuildConfig.DEBUG)
+                // 设置是否输出到控制台开关，默认开
+                .setConsoleSwitch(BuildConfig.DEBUG)
+                // 设置 log 全局标签，默认为空
+                // 当全局标签不为空时，我们输出的 log 全部为该 tag，
+                // 为空时，如果传入的 tag 为空那就显示类名，否则显示 tag
+                .setGlobalTag(null)
+                // 设置 log 头信息开关，默认为开
+                .setLogHeadSwitch(true)
+                // 打印 log 时是否存到文件的开关，默认关
+                .setLog2FileSwitch(false)
+                // 输出日志是否带边框开关，默认开
+                .setBorderSwitch(true)
+                // 一条日志仅输出一条，默认开，为美化 AS 3.1 的 Logcat
+                .setSingleTagSwitch(true)
+                // log 的控制台过滤器，和 logcat 过滤器同理，默认 Verbose
+                .setConsoleFilter(LogUtils.V)
+                // log 文件过滤器，和 logcat 过滤器同理，默认 Verbose
+                .setFileFilter(LogUtils.V);
     }
 
     @NonNull
--- app/src/main/java/com/lucas/ebo/ui/activity/splash/SplashLoginActivity.java
+++ app/src/main/java/com/lucas/ebo/ui/activity/splash/SplashLoginActivity.java
@@ -12,6 +12,7 @@ import android.view.animation.TranslateAnimation;
 
 import androidx.databinding.DataBindingUtil;
 
+import com.lucas.architecture.utils.LogUtils;
 import com.lucas.ebo.R;
 import com.lucas.ebo.bridge.state.SplashLoginActivityViewModel;
 import com.lucas.ebo.databinding.ActivitySplashLoginBinding;
@@ -98,20 +99,6 @@ public class SplashLoginActivity extends BaseActivity {
         mBinding.splashVideo.setOnInfoListener(new MediaPlayer.OnInfoListener() {
             @Override
             public boolean onInfo(MediaPlayer mp, int what, int extra) {
-                //信息回调
-                //                what 对应返回的值如下
-                //                public static final int MEDIA_INFO_UNKNOWN = 1;  媒体信息未知
-                //                public static final int MEDIA_INFO_VIDEO_TRACK_LAGGING = 700; 媒体信息视频跟踪滞后
-                //                public static final int MEDIA_INFO_VIDEO_RENDERING_START = 3; 媒体信息\视频渲染\开始
-                //                public static final int MEDIA_INFO_BUFFERING_START = 701; 媒体信息缓冲启动
-                //                public static final int MEDIA_INFO_BUFFERING_END = 702; 媒体信息缓冲结束
-                //                public static final int MEDIA_INFO_NETWORK_BANDWIDTH = 703; 媒体信息网络带宽（703）
-                //                public static final int MEDIA_INFO_BAD_INTERLEAVING = 800; 媒体-信息-坏-交错
-                //                public static final int MEDIA_INFO_NOT_SEEKABLE = 801; 媒体信息找不到
-                //                public static final int MEDIA_INFO_METADATA_UPDATE = 802; 媒体信息元数据更新
-                //                public static final int MEDIA_INFO_UNSUPPORTED_SUBTITLE = 901; 媒体信息不支持字幕
-                //                public static final int MEDIA_INFO_SUBTITLE_TIMED_OUT = 902; 媒体信息字幕超时
-                //如果方法处理了信息，则为true；如果没有，则为false。返回false或根本没有OnInfoListener，将导致丢弃该信息。
                 return false;
             }
         });
@@ -124,13 +111,13 @@ public class SplashLoginActivity extends BaseActivity {
         }
 
         public void signUpClick() {
-            Log.i(TAG, "signUpClick: ");
+            LogUtils.dTag(TAG, "点击signUpClick");
             Intent intent = new Intent(SplashLoginActivity.this, SignUpActivity.class);
             startActivity(intent);
         }
 
         public void loginClick() {
-            Log.i(TAG, "loginClick: ");
+            LogUtils.dTag(TAG, "loginClick");
             Intent intent = new Intent(SplashLoginActivity.this, LoginActivity.class);
             startActivity(intent);
         }
